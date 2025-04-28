0febced79aa88b76322277d4cd151c5509b749d9
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-28 14:58:30 -0500

    Create  controlador_vivero_vital.py

---
+++ controlador_vivero_vital.py
@@ -0,0 +1,18 @@
+class controlador:
+
+    def __init__(self):
+        self.modelo.modelo_vivero_vital()
+        self.obj=controlador()
+        
+    def guardar_datos(self, datos):
+       self.modelo.guardar_datos(datos)
+
+
+    def Buscar_datos(self,datos):
+       self.modelo.Buscar_datos(datos)
+
+    def Actualizar_datos(self,datos):
+        self.modelo.Actualizar_datos(datos)
+
+    def Eliminar_datos(self,datos):
+        self.modelo.Eliminar_datos(datos)
