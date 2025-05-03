a52eaf397a7965a0297bd701a02c745d185c7770
Esteban-bit-dot <eandrescardales@gmail.com>
2025-05-03 15:42:15 -0500

    Update modelo_vivero_vital.py

--- modelo_vivero_vital.py
+++ modelo_vivero_vital.py
@@ -1,16 +1,19 @@
-import Modelo as Vista
-class modelo_vivero_vital:
-     
+import modelo as controlador
+
+class modelo:
+
+  def __init__ (self, datocolor, datoforma, datotamaño):
+    pass
 # abctraccion
 
- class vivero_vital:
+class vivero_vital:
 
-  def __init__ (self,datocolor,datoforma,datotamaño,datoplanta):
+  def __init__ (self, datocolor, datoforma, datotamaño):
     self.color=""
     self.forma=""
     self.tamaño=""
 
-  def asignacion_material(self, datocolor,datoforma,datotamaño):
+  def asignacion_material(self, datocolor, datoforma, datotamaño):
     self.color=datocolor
     self.forma=datoforma
     self.tamaño=datotamaño
@@ -18,13 +21,14 @@ class modelo_vivero_vital:
     
 # encapsulamiento
 
-class vivero:
-  def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
+class vivero_vital:
+
+  def __init__(self, datocolor, datoforma, datotamaño, datoplanta):
     self.color=""
     self.forma=""
     self.tamaño=""
 
-  def asignacion_material(self, datocolor,datoforma,datotamaño):
+  def asignacion_material(self, datocolor, datoforma, datotamaño):
     self.color=datocolor
     self.forma=datoforma
     self.tamaño=datotamaño
@@ -33,12 +37,13 @@ class vivero:
 # Herencia
 
   class vivero_vital:
+   
    def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
     self.color=""
     self.forma=""
     self.tamaño=""
     
-   def asignacion_material(self, datocolor,datoforma,datotamaño):
+   def asignacion_material(self, datocolor, datoforma, datotamaño):
     self.color=datocolor
     self.forma=datoforma
     self.tamaño=datotamaño
@@ -46,50 +51,53 @@ class vivero:
     
 
   class vivero_hijo1:
-   def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
-    super(). __init__()
-    self.color=""
-    self.forma=""
-    self.tamaño=""
-    self.planta=""
-
-  def asignacion_material(self,datocolor,datoforma,datotamaño,datoplanta):
-    super().asignacion_material(datocolor,datoforma,datotamaño,datoplanta)
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.planta=datoplanta
-    print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
-    
-
-  class vivero_hijo2:
-   def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
-    super(). __init__()
-    self.color=""
-    self.forma=""
-    self.tamaño=""
-    self.planta=""
-
-  def asignacion_material2(self,datocolor,datoforma,datotamaño,datoplanta):
-    super().asignacion_material(datocolor,datoforma,datotamaño,datoplanta)
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.planta=datoplanta
-    print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
-    
-#polimorfismo
-
-  class vivero_hijo1:
-   def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
+   
+   def __init__(self, datocolor, datoforma, datotamaño,datoplanta):
      super(). __init__(self)
      self.color=""
      self.forma=""
      self.tamaño=""
      self.planta=""
 
-  def asignacion_material1(self,datocolor,datoforma,datotamaño,datoplanta):
-    super().asignacion_material1(datocolor,datoforma,datotamaño,datoplanta)
+   def asignacion_material(self, datocolor, datoforma, datotamaño, datoplanta):
+     super().asignacion_material(datocolor, datoforma, datotamaño, datoplanta)
+     self.color=datocolor
+     self.forma=datoforma
+     self.tamaño=datotamaño
+     self.planta=datoplanta
+     print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
+    
+
+  class vivero_hijo2:
+   
+   def __init__(self, datocolor, datoforma, datotamaño, datoplanta):
+     super(). __init__(self)
+     self.color=""
+     self.forma=""
+     self.tamaño=""
+     self.planta=""
+
+   def asignacion_material2(self, datocolor, datoforma, datotamaño, datoplanta):
+     super().asignacion_material(datocolor, datoforma, datotamaño, datoplanta)
+     self.color=datocolor
+     self.forma=datoforma
+     self.tamaño=datotamaño
+     self.planta=datoplanta
+     print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
+    
+#polimorfismo
+
+class vivero_hijo1:
+   
+  def __init__(self, datocolor, datoforma, datotamaño, datoplanta):
+     super(). __init__(self)
+     self.color=""
+     self.forma=""
+     self.tamaño=""
+     self.planta=""
+
+  def asignacion_material1(self, datocolor, datoforma, datotamaño, datoplanta):
+    super().asignacion_material1(datocolor, datoforma, datotamaño, datoplanta)
     self.color=datocolor
     self.forma=datoforma
     self.tamaño=datotamaño
@@ -97,90 +105,104 @@ class vivero:
     print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
     
 
-  class vivero_hijo2:
+class vivero_hijo2:
 
-   def __init__(self,datocolor,datoforma,datotamaño,datoplanta):
+   def __init__(self, datocolor, datoforma, datotamaño, datoplanta):
     super().__init__(self)
     self.color=""
     self.forma=""
     self.tamaño=""
     self.planta=""
 
-  def asignacion_material2(self, datocolor, datoforma, datotamaño, datoplanta):
-    super().asignacion_material2(datocolor, datoforma, datotamaño, datoplanta)
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.planta=datoplanta
-    print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
+   def asignacion_material2(self, datocolor, datoforma, datotamaño, datoplanta):
+     super().asignacion_material2(datocolor, datoforma, datotamaño, datoplanta)
+     self.color=datocolor
+     self.forma=datoforma
+     self.tamaño=datotamaño
+     self.planta=datoplanta
+     print(f"los viveros son: {self.color}- de forma {self.forma}- de tamaño {self.tamaño} y tiene{self.planta}")
    
 
-  def guardar_datos(self,Auxlista):
-    self.Herencia=Auxlista[0]
+def guardar_datos(self, Auxlista):
+    self.Herencia = Auxlista[0]
 
-def buscar_datos(self,dato):
-    if dato == self.datos():
+def buscar_datos(self, datos):
+    if datos == self.datos():
        print("encontrado")
     else:
        print("no exite")
 
-def actualizar(self, dato):
-  if dato == self.datos():
-    if dato == vivero_vital:
+def actualizar_datos(self, datos):
+  if datos == self.datos():
+    if datos == vivero_vital:
       print ("ingrese los datos que va actualizar: ")
   else:
      print("error")
 
-  vivero_vital = vivero
-while vivero =="2":
+def eliminar_datos(self, datos):
+   if datos == self.datos():
+     if datos == vivero_vital:
+      print ("ingrese los datos que va eliminar: ")
+   else:
+     print("error")
+
+while vivero_vital == 2:
      vivero_vital = 2 
-     print(vivero)
+     print(vivero_vital)
      if vivero_vital == 0:
       print("error")
       break
      else:
         print("fin del bucle") 
 
-def Guardar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.plantas= datoplanta
+#CRUD
+class Vivero:
+ 
+ def __init__(self, datocolor, datoforma, datotamaño, datoplanta):
+      self.datocolor = datocolor
+      self.datoforma = datoforma
+      self.datotamaño = datotamaño
+      self.datoplanta = datoplanta
 
-def Actualizar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.plantas= datoplanta
+ def Guardar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
+    self.color = datocolor
+    self.forma = datoforma
+    self.tamaño = datotamaño
+    self.plantas = datoplanta
+
+ def Actualizar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
+    self.color = datocolor
+    self.forma = datoforma
+    self.tamaño = datotamaño
+    self.plantas = datoplanta
     self.actualizar_datos = (datocolor, datoforma, datotamaño, datoplanta)
 
-def Eliminar_datos(self,datocolor,datoforma,datotamaño, datoplanta):
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.plantas= datoplanta
-    self.Eliminar_datos = (datocolor,datoforma,datotamaño, datoplanta)
+ def Eliminar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
+    self.color = datocolor
+    self.forma = datoforma
+    self.tamaño = datotamaño
+    self.plantas = datoplanta
+    self.Eliminar_datos = (datocolor, datoforma, datotamaño, datoplanta)
 
-def Mostrar_datos(self,datocolor,datoforma,datotamaño, datoplanta):
-    tuple == (datocolor,datoforma,datotamaño, datoplanta)
-    self.color=datocolor
-    self.forma=datoforma
-    self.tamaño=datotamaño
-    self.plantas= datoplanta
-    self.Mostrar_datos = (datocolor,datoforma,datotamaño, datoplanta)
+ def Mostrar_datos(self, datocolor, datoforma, datotamaño, datoplanta):
+    tuple == (datocolor, datoforma, datotamaño, datoplanta)
+    self.color = datocolor
+    self.forma = datoforma
+    self.tamaño = datotamaño
+    self.plantas = datoplanta
+    self.Mostrar_datos = (datocolor, datoforma, datotamaño, datoplanta)
 
-def setdatos(self, datos):
-   datos = self.datos()
+def set_datos(self, datos):
+    datos = self.datos();
 
-def getdatos(datos):
-     return datos();
+def get_datos(self, datos):
+    return datos();
 
 #******************************Codigo Pricipal***************************************#
-
-objvivero_vital=vivero("marron","rentagulo","grande","rojo")
-objvivero_hijo1=vivero("marron claro","retangulo","mediano","plantas")
-objvivero_hijo2=vivero("marron claro","retangulo","mediano","plantas")
-objvivero_vital.Guardar_datos("marron claro","retangulo","mediano","plantas")
-Guardar_datos.vivero_vital=Actualizar_datos("marron claro","retangulo","mediano","plantas")
-Eliminar_datos.objvivero_vital("marron claro","retangulo","mediano","plantas")
-Mostrar_datos("marron claro","retangulo","mediano","plantas")
+objvivero_vital=Vivero("marron", "rentagulo", "grande", "rojo")
+objvivero_hijo1=Vivero("marron claro", "retangulo", "mediano", "plantas")
+objvivero_hijo2=Vivero("marron claro", "retangulo", "mediano", "plantas")
+objvivero_vital.Guardar_datos("marron claro", "retangulo", "mediano", "plantas")
+objvivero_vital.Actualizar_datos("marron claro", "retangulo", "mediano", "plantas")
+objvivero_vital.Eliminar_datos("marron claro", "retangulo", "mediano", "plantas")
+objvivero_vital.Mostrar_datos("marron claro", "retangulo", "mediano", "plantas")
